636c25c43647b38c33f0145eef7e6973a21ebf1a
onatyr <onat.rigault@gmail.com>
2023-12-02 17:36:23 +0100

    cubeconundrum pt2

--- src/main/kotlin/2_CubeConundrum.kt
+++ src/main/kotlin/2_CubeConundrum.kt
@@ -1,14 +1,32 @@
 import java.io.File
 
+fun powerOfSets(line: String): Int {
+    val inCube = line.split("Game ")[1].split(": ")[1].split(";")
+
+    val minValues = mutableMapOf<String, Int>("red" to 1, "green" to 1, "blue" to 1)
+
+    inCube.map { set ->
+        for (s in set.split(", ")) {
+            val newS = s.split(" ").filter { it.isNotEmpty() }
+            val number = newS[0].toInt()
+            val color = newS[1]
+            if (minValues[color]!! < number){
+                minValues[color] = number
+            }
+        }
+    }
+
+    return minValues["red"]!! * minValues["green"]!! *minValues["blue"]!!
+}
+
 fun checkPossible(line: String): Int {
     val possibilities = mapOf<String, Int>("red" to 12, "green" to 13, "blue" to 14)
     val gameId = line.split("Game ")[1].split(": ")[0].toInt()
     val inCube = line.split("Game ")[1].split(": ")[1].split(";")
-    println(line)
-    println(inCube)
+
     inCube.map { set ->
         for (s in set.split(", ")) {
-            val newS = s.split(" ").filter { it.length > 0 }
+            val newS = s.split(" ").filter { it.isNotEmpty() }
             val number = newS[0].toInt()
             val color = newS[1]
             if (possibilities[color]!! < number) {
@@ -23,7 +41,8 @@ fun checkPossible(line: String): Int {
 fun cubeConundrum(): Int {
     var accu = 0
     File("src/main/kotlin/cubeInput").forEachLine {
-        accu += checkPossible(it)
+//        accu += checkPossible(it)
+        accu += powerOfSets(it)
     }
     return accu
 }
