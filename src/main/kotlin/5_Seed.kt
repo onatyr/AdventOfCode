a031b705f4064e4a842d99b8ec1a9da260bae1bb
onatyr <onat.rigault@gmail.com>
2023-12-11 13:43:35 +0100

    day 5 pt2

--- src/main/kotlin/5_Seed.kt
+++ src/main/kotlin/5_Seed.kt
@@ -4,14 +4,22 @@ data class Seed(val number: Long, var location: Long)
 fun seedLocation(seed: Seed, parsers: List<ParsedMap>): Long {
     var seedNumber = seed.number
     for(parser in parsers){
-        seedNumber = parser.convert(seedNumber)
+        seedNumber = parser.convertToDestination(seedNumber)
     }
     return seedNumber
 }
+
+fun locationToSeed(location: Long, parsers: List<ParsedMap>): Long {
+    var sourceNumber = location
+    for(parser in parsers.reversed()){
+        sourceNumber = parser.convertToSource(sourceNumber)
+    }
+    return sourceNumber
+}
 class ParsedMap(private val input: String, private val startDelimiter: String, private val endDelimiter: String) {
 
-    private val sourceRanges = parseRanges(1)
-    private val destinationRanges = parseRanges(0)
+    val sourceRanges = parseRanges(1)
+    val destinationRanges = parseRanges(0)
 
     private fun parseRanges(startColumn: Int): List<LongRange> {
         val rangeStrings = input.split("$startDelimiter:\n").last().split(
@@ -20,10 +28,18 @@ class ParsedMap(private val input: String, private val startDelimiter: String, p
         return rangeStrings.map { LongRange(it[startColumn].toLong(), it[startColumn].toLong() + it[2].toLong() -1) }
     }
 
-    fun convert(number: Long): Long{
+    fun convertToDestination(number: Long): Long{
         for(rangeIndex in sourceRanges.indices){
             if (number in sourceRanges[rangeIndex]){
                 return number-(sourceRanges[rangeIndex].first()-destinationRanges[rangeIndex].first())
+            }
+        }
+        return number
+    }
+    fun convertToSource(number: Long): Long{
+        for(rangeIndex in destinationRanges.indices){
+            if (number in destinationRanges[rangeIndex]){
+                return number-(destinationRanges[rangeIndex].first()-sourceRanges[rangeIndex].first())
             }
         }
         return number
@@ -39,14 +55,38 @@ fun seedLocation(): Long {
     allLines += "\nend"
 
     val delimiters = allLines.split("\n\n").map { it.split(":").first() }.filter { it != "seeds" }
-    println(delimiters)
     val listParser: MutableList<ParsedMap> = emptyList<ParsedMap>().toMutableList()
     for (i in 0..<delimiters.size-1) {
         listParser.add(ParsedMap(allLines, delimiters[i], delimiters[i+1]))
     }
-    val seedsList = allLines.split("seeds: ").last().split("\n\n").first().split(" ").map { Seed(it.toLong(), -1) }.toMutableList()
-    seedsList.forEach { it.location = seedLocation(it, listParser) }
-    seedsList.sortBy { it.location }
+//    val seedsListPt1 = allLines.split("seeds: ").last().split("\n\n").first().split(" ").map { Seed(it.toLong(), -1) }.toMutableList()
+//    seedsListPt1.forEach { it.location = seedLocation(it, listParser) }
+//    seedsListPt1.sortBy { it.location }
+//    return seedsListPt1.first().location
 
-    return seedsList.first().location
+    val seedsString = allLines.split("seeds: ").last().split("\n\n").first().split(" ")
+    val seedsRangeListPt2: MutableList<LongRange> = emptyList<LongRange>().toMutableList()
+    for (i in 0..<seedsString.size-1 step 2){
+        val newRange = LongRange(seedsString[i].toLong(),seedsString[i].toLong() + seedsString[i+1].toLong())
+        seedsRangeListPt2.add(newRange)
+    }
+//    println(seedsList.size)
+
+    var continueLoop = true
+    var init = 46.toLong()
+    while(continueLoop){
+        val seedNumber = locationToSeed(init, listParser)
+        for (range in seedsRangeListPt2){
+            if(seedNumber in range){
+                continueLoop = false
+                break
+            }
+        }
+        init += 1
+    }
+    
+
+
+return init-1
+
 }
